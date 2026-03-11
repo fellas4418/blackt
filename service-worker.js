11167e35a1da3cd6e777ad5de07e50c535fdea87
Shinuk <118554435+fellas4418@users.noreply.github.com>
2026-03-12 01:48:29 +0900

    fix: 진짜 서비스워커 자폭 코드 투입

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,17 @@
-const CACHE_NAME = 'trigger-voca-v1';
-const urlsToCache = [
-    './',
-    './index.html',
-    './study.html',
-    './style.css',
-    './app.js',
-    './wordData.js'
-];
-
-// 1. 앱이 처음 켜질 때 필요한 파일들을 폰에 저장(캐싱)
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
 });
 
-// 2. 인터넷이 끊겼을 때 서버 대신 폰에 저장된 파일을 꺼내줌
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response; // 폰에 저장된 캐시에서 꺼냄
-                }
-                return fetch(event.request); // 캐시에 없으면 인터넷에서 가져옴
-            })
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    return caches.delete(cacheName); 
+                })
+            );
+        }).then(() => {
+            self.registration.unregister(); 
+        })
     );
 });
